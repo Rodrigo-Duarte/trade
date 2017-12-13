f1b60e3de5a46653ce3daa433aaba1a99336e40a
Rodrigo Duarte <rduarte@thoughtworks.com>
2017-12-13 16:29:39 -0200

    Rolling up barriers

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 var BtcRead = require('./mercado_bitcoin_read.js')
 var dashboard = require('./ui.js')
 
+function barrierSum(allOrders, barriers) {
+    var asksToConsume = allOrders.slice()
+    var accumulatedQty = []
+    for ( i = 0 ; i < barriers.length ; i++) {
+        var ind = asksToConsume.findIndex(a => a[0] == barriers[i][0])
+        var totalVolumeUpToBarrier = asksToConsume.slice(0, ind + 1).map(a => a[1]).reduce((a, b) => a + b, 0)
+        // console.log(`Group ${i} (${barriers[i]}): ` + JSON.stringify(asksToConsume.slice(0, ind + 1)) + " = " + totalVolumeUpToBarrier)
+        accumulatedQty.push([barriers[i][0], totalVolumeUpToBarrier])
+        asksToConsume = asksToConsume.splice(ind+1)
+    }
+    return accumulatedQty
+}
+
 function createCoinDashboard(coin, anchor){
     var btc = new BtcRead(coin)
 
@@ -19,24 +32,27 @@ function createCoinDashboard(coin, anchor){
     var upperBarrierUi = dashboard.addUpperBarriers(anchor+1,1, coin)
     var lowerBarrierUi = dashboard.addLowerBarriers(anchor+1,0, coin)
     btc.registerOrderbookListener(function(ina) {
-        var margin = 0.3
+        var margin = 0.15
         var barrierCount = 7
 
         var asksHorizon = ina.asks.filter(x => x[0] <= ina.asks[0][0] * (1 + margin))
         var bidsHorizon = ina.bids.filter(x => x[0] >= ina.bids[0][0] * (1 - margin))
 
-        var lowerBarriers = bidsHorizon.sort((a,b) => b[1]-a[1]).slice(0,barrierCount).sort((a,b) => a[0]-b[0])
-        var upperBarriers = asksHorizon.sort((a,b) => b[1]-a[1]).slice(0,barrierCount).sort((a,b) => a[0]-b[0])
+        var upperBarriers = asksHorizon.slice().sort((a,b) => b[1]-a[1]).slice(0,barrierCount).sort((a,b) => b[0]-a[0])
+        var lowerBarriers = bidsHorizon.slice().sort((a,b) => b[1]-a[1]).slice(0,barrierCount).sort((a,b) => a[0]-b[0])
 
-        dashboard.updateBarriers(lowerBarrierUi,
-        {
-            titles: lowerBarriers.map(x => "R$ " + x[0]),
-            data: lowerBarriers.map(x => x[1].toFixed(4))
-        })
+        var accUpperBar = barrierSum(asksHorizon, upperBarriers.slice().reverse())
+        var accLowerBar = barrierSum(bidsHorizon, lowerBarriers.slice().reverse()).reverse()
+        
         dashboard.updateBarriers(upperBarrierUi,
         {
-            titles: upperBarriers.map(x => "R$ " + x[0]),
-            data: upperBarriers.map(x => x[1].toFixed(4))
+            titles: accUpperBar.map(x => "R$ " + x[0].toFixed(2)),
+            data: accUpperBar.map(x => x[1].toFixed(3))
+        })
+        dashboard.updateBarriers(lowerBarrierUi,
+        {
+            titles: accLowerBar.map(x => "R$ " + x[0].toFixed(2)),
+            data: accLowerBar.map(x => x[1].toFixed(3))
         })
     })
 
